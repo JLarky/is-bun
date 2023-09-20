2cc882dc4cec21780d27c3b65c0e98bd49cd143c
JLarky <jlarky@gmail.com>
2023-09-20 17:55:49 -0600

    publish npm

---
+++ _build_npm.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-env --allow-run
+// Copyright 2018-2022 the oak authors. All rights reserved. MIT license.
+
+/**
+ * This is the build script for building npm package.
+ *
+ * @module
+ */
+
+import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
+
+async function start() {
+  await emptyDir("./npm");
+
+  await build({
+    entryPoints: ["./mod.ts"],
+    outDir: "./npm",
+    shims: {},
+    test: false,
+    typeCheck: "both",
+    compilerOptions: {
+      importHelpers: false,
+      sourceMap: true,
+      target: "ES2021",
+      lib: ["esnext", "dom", "dom.iterable"],
+    },
+    package: {
+      name: "is-bun",
+      version: Deno.args[0],
+      description: "Return true if you are running in Bun.",
+      license: "MIT",
+      keywords: ["bun"],
+      engines: {
+        node: ">=8.0.0",
+      },
+      repository: {
+        type: "git",
+        url: "git+https://github.com/JLarky/is-bun.git",
+      },
+      bugs: {
+        url: "https://github.com/JLarky/is-bun/issues",
+      },
+      dependencies: {},
+      devDependencies: {},
+    },
+  });
+
+  await Deno.copyFile("LICENSE", "npm/LICENSE");
+  await Deno.copyFile("README.md", "npm/README.md");
+}
+
+start();
