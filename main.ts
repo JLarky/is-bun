7fd9670dfe3fd572eb091d7fdac3e83490aa456a
JLarky <jlarky@gmail.com>
2023-09-20 17:42:29 -0600

    we wrote our isBun module for Deno

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function isBun(): boolean {
+  // I didn't want to import bun-types package
+  // @ts-ignore
+  return typeof Bun !== "undefined";
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("In Deno isBun is", isBun());
 }
--- main_bench.ts
+++ main_bench.ts
@@ -1,9 +1,5 @@
-import { add } from "./main.ts";
+import { isBun } from "./main.ts";
 
-Deno.bench(function addSmall() {
-  add(1, 2);
-});
-
-Deno.bench(function addBig() {
-  add(2 ** 32, 2 ** 32);
+Deno.bench(function benchIsBun() {
+  isBun();
 });
